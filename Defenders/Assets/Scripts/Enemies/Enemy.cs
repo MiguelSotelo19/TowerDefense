a12e03a8a2d35f674cc945137fdb960db151a15e
Fernanda 260504 <20223tn086@utez.edu.mx>
2025-11-13 18:24:21 -0600

    IN-16 Modelos de Virus Básico y Archivo Corrupto

--- Defenders/Assets/Scripts/Enemies/Enemy.cs
+++ Defenders/Assets/Scripts/Enemies/Enemy.cs
@@ -1,10 +1,60 @@
 using UnityEngine;
+using System.Collections;
+using UnityEngine.Splines;
 
 public class Enemy : MonoBehaviour
 {
+    [Header("Animation Settings")]
+    [SerializeField] private Animator animator;
+    [Tooltip("Tiempo de espera para que se reproduzca la animación de muerte antes de devolverlo al Pool.")]
+    [SerializeField] private float deathAnimationDuration = 2.0f;
+
+    [Header("Enemy Stats")]
     [SerializeField] private int damageToCore = 1;
-    [HideInInspector] public SpawnPoint ownerSpawner;
     public int bytes = 0;
+
+    [HideInInspector] public SpawnPoint ownerSpawner;
+
+    private Health enemyHealth;
+    private FollowPathAgent followPathAgent;
+
+    private void Awake()
+    {
+        if (animator == null)
+        {
+            animator = GetComponent<Animator>();
+        }
+        enemyHealth = GetComponent<Health>();
+        followPathAgent = GetComponent<FollowPathAgent>();
+    }
+
+    private void OnEnable()
+    {
+        if (animator != null)
+        {
+            animator.SetBool("IsDead", false);
+        }
+
+        if (enemyHealth != null)
+        {
+            enemyHealth.onDeath.AddListener(Die);
+            enemyHealth.onHealthChange.AddListener(HandleHealthChange);
+        }
+
+        if (followPathAgent != null)
+        {
+            followPathAgent.enabled = true;
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (enemyHealth != null)
+        {
+            enemyHealth.onDeath.RemoveListener(Die);
+            enemyHealth.onHealthChange.RemoveListener(HandleHealthChange);
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -12,6 +62,14 @@ public class Enemy : MonoBehaviour
         {
             Debug.Log("Llego a core");
             ReachCore();
+        }
+    }
+
+    private void HandleHealthChange(float current, float max)
+    {
+        if (current > 0 && animator != null)
+        {
+            animator.SetTrigger("Hit");
         }
     }
 
@@ -23,18 +81,42 @@ public class Enemy : MonoBehaviour
             core.TakeDamage(damageToCore);
         }
 
-        ReturnToPoolOrDisable();
+        Die();
     }
 
     public void Die()
     {
-        //Aqui vas a hacer lo de animaciones por si mueren o alguna locochoneria asi
+        if (animator != null && animator.GetBool("IsDead")) return;
+
+        if (followPathAgent != null)
+        {
+            followPathAgent.enabled = false;
+        }
+
         EconomyManager.Instance.AddBytes(bytes);
+
+        if (animator != null)
+        {
+            animator.SetBool("IsDead", true);
+        }
+
+        StartCoroutine(WaitAndReturnToPool(deathAnimationDuration));
+    }
+
+    private IEnumerator WaitAndReturnToPool(float delay)
+    {
+        yield return new WaitForSeconds(delay);
         ReturnToPoolOrDisable();
     }
 
     private void ReturnToPoolOrDisable()
     {
+        if (followPathAgent != null)
+        {
+            followPathAgent.enabled = true;
+            followPathAgent.ResetProgress(false);
+        }
+
         if (ownerSpawner != null)
         {
             ownerSpawner.ReturnToPool(gameObject);
@@ -48,5 +130,11 @@ public class Enemy : MonoBehaviour
     public void Initialize(SpawnPoint spawner)
     {
         ownerSpawner = spawner;
+
+        if (followPathAgent != null)
+        {
+            followPathAgent.enabled = true;
+            followPathAgent.ResetProgress(false);
+        }
     }
 }
